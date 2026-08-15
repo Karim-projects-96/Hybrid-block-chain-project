c41cf4b858844e324fee358d0f085d10ac61e6a0
Karim-projects-96 <k72786786@gmail.com>
2026-08-15 09:25:49 +0530

    sell function update

--- user/transfer_ownership.php
+++ user/transfer_ownership.php
@@ -30,8 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $sql = "UPDATE jewellery SET current_owner_id = $new_owner_id WHERE token_id = $token_id";
                 if ($conn->query($sql) === TRUE) {
                     $tx_hash = md5($token_id . $user_id . $new_owner_id . time());
-                    $get_j = $conn->query("SELECT id FROM jewellery WHERE token_id = $token_id")->fetch_assoc();
-                    $jid = $get_j['id'];
+                    $jid = $item['id'];
                     $conn->query("INSERT INTO transactions (jewellery_id, from_user_id, to_user_id, tx_hash) VALUES ($jid, $user_id, $new_owner_id, '$tx_hash')");
                     $success = "Ownership transferred successfully!";
                     
