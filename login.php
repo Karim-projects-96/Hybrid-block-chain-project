39d609259318dd9c923664593a036f581d746475
Karim-projects-96 <k72786786@gmail.com>
2026-08-21 10:23:49 +0530

    underline remove

--- login.php
+++ login.php
@@ -73,7 +73,7 @@ if (isset($_GET['role'])) {
             <label>Password</label>
             <input type="password" name="password" class="form-control" required placeholder="Enter your password">
             <div style="text-align: right; margin-top: 0.5rem;">
-                <a href="forgot_password.php" style="color: var(--primary-gold); font-size: 0.9rem;">Forgot Password?</a>
+                <a href="forgot_password.php"  style="color: var(--primary-gold); font-size: 0.9rem;text-decoration:none; ">Forgot Password?</a>
             </div>
         </div>
         <button type="submit" class="btn btn-gold" style="width: 100%; margin-top: 1rem;">Login</button>
